5dba22054002de3d922fbe0f583ffe4a8ff510bc
Sakiyama Shingo <yamaa.exception@gmail.com>
2024-08-10 23:47:32 +0900

    chore: モノレポのためにESLintの設定を共通化

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,4 @@
-import { common } from '@agaroot/eslint-config-common';
-import { define } from '@agaroot/eslint-config-definer';
-import { javascript } from '@agaroot/eslint-config-javascript';
-import { style } from '@agaroot/eslint-config-style';
-import { typescript } from '@agaroot/eslint-config-typescript';
-
-export const config = define([
-  common,
-  javascript,
-  typescript,
-  style,
-]);
+import { config } from '@ur-automation/config-common/eslint.config.mjs';
 
 export default config({
   tsconfigPath: './tsconfig.json',
