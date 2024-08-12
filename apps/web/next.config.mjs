a55fc60d31183ab4621e673886e75c57a8153182
Sakiyama Shingo <yamaa.exception@gmail.com>
2024-08-12 12:26:08 +0900

    feature: drizzleを追加＆テーブルを1つ追加＆マイグレートのCI/CDを追加

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -4,7 +4,11 @@ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 // (when running the application with `next dev`), for more information see:
 // https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
 if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
+  await setupDevPlatform({
+    persist: {
+      path: '../../packages/.wrangler/',
+    },
+  });
 }
 
 /** @type {import('next').NextConfig} */
---
+++ packages/database/eslint.config.mjs
@@ -0,0 +1,12 @@
+import { commonConfig } from '@ur-automation/config-common/eslint.config.mjs';
+
+export default [
+  ...commonConfig({
+    tsconfigPath: './tsconfig.json',
+  }),
+  {
+    ignores: [
+      'drizzle/',
+    ],
+  },
+];
