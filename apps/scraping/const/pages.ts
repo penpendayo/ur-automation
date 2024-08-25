1e2b1970178b56e4188661757045b6ea78b3629c
Sakiyama Shingo <yamaa.exception@gmail.com>
2024-08-25 22:24:11 +0900

    feat: スクレイピング対象のページを追加

---
+++ apps/scraping/const/pages.ts
@@ -0,0 +1,94 @@
+export const pages = [
+  // 近畿
+  {
+    prefecture: '大阪府',
+    url: 'https://www.ur-net.go.jp/chintai/kansai/osaka/list/',
+  },
+  {
+    prefecture: '和歌山県',
+    url: 'https://www.ur-net.go.jp/chintai/kansai/wakayama/list/',
+  },
+  {
+    prefecture: '滋賀県',
+    url: 'https://www.ur-net.go.jp/chintai/kansai/shiga/list/',
+  },
+  {
+    prefecture: '兵庫県',
+    url: 'https://www.ur-net.go.jp/chintai/kansai/hyogo/list/',
+  },
+  {
+    prefecture: '奈良県',
+    url: 'https://www.ur-net.go.jp/chintai/kansai/nara/list/',
+  },
+  {
+    prefecture: '京都府',
+    url: 'https://www.ur-net.go.jp/chintai/kansai/kyoto/list/',
+  },
+  // 中国
+  {
+    prefecture: '山口県',
+    url: 'https://www.ur-net.go.jp/chintai/chugoku/yamaguchi/',
+  },
+  {
+    prefecture: '岡山県',
+    url: 'https://www.ur-net.go.jp/chintai/chugoku/okayama/',
+  },
+  {
+    prefecture: '広島県',
+    url: 'https://www.ur-net.go.jp/chintai/chugoku/hiroshima/',
+  },
+  // 九州
+  {
+    prefecture: '福岡県',
+    url: 'https://www.ur-net.go.jp/chintai/kyushu/fukuoka/list/',
+  },
+
+  // 北海道・東北
+  {
+    prefecture: '北海道',
+    url: 'https://www.ur-net.go.jp/chintai/hokkaitohoku/hokkaido/list/',
+  },
+  {
+    prefecture: '宮城県',
+    url: 'https://www.ur-net.go.jp/chintai/hokkaitohoku/miyagi/list/',
+  },
+
+  // 関東
+  {
+    prefecture: '東京都',
+    url: 'https://www.ur-net.go.jp/chintai/kanto/tokyo/list/',
+  },
+  {
+    prefecture: '神奈川県',
+    url: 'https://www.ur-net.go.jp/chintai/kanto/kanagawa/list/',
+  },
+  {
+    prefecture: '千葉県',
+    url: 'https://www.ur-net.go.jp/chintai/kanto/chiba/list/',
+  },
+  {
+    prefecture: '埼玉県',
+    url: 'https://www.ur-net.go.jp/chintai/kanto/saitama/list/',
+  },
+  {
+    prefecture: '茨城県',
+    url: 'https://www.ur-net.go.jp/chintai/kanto/ibaraki/list/',
+  },
+  // 東海
+  {
+    prefecture: '愛知県',
+    url: 'https://www.ur-net.go.jp/chintai/tokai/aichi/list/',
+  },
+  {
+    prefecture: '三重県',
+    url: 'https://www.ur-net.go.jp/chintai/tokai/mie/list/',
+  },
+  {
+    prefecture: '岐阜県',
+    url: 'https://www.ur-net.go.jp/chintai/tokai/gifu/list/',
+  },
+  {
+    prefecture: '静岡県',
+    url: 'https://www.ur-net.go.jp/chintai/tokai/shizuoka/list/',
+  },
+] as const;
